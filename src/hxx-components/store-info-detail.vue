2b899d049fff79910a5c75c6172d3ae33cb49796
lanto <lanto@lantodeMacBook.local>
2018-09-03 18:32:12 +0800

    store-info

--- src/hxx-components/store-info-detail.vue
+++ src/hxx-components/store-info-detail.vue
@@ -7,10 +7,52 @@
         <FormItem prop="COMPANYCODE" label="门店名称">
           <Input v-model="info.COMPANYCODE"></Input>
         </FormItem>
-        <FormItem prop="COMPANYCODE" label="企业编码">
+        <FormItem prop="COMPANYCODE" label="门店所属区域">
           <Input v-model="info.COMPANYCODE"></Input>
         </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="登录密码">
+        <FormItem prop="COMPANYPASSWORD" label="门店地址">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="经营业务类别">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="经营范围">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="统一社会信用代码">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="道路运输许可证号">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="许可证开始日期">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="许可证结束日期">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="负责人姓名">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="负责人联系方式">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="联系人姓名">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="联系人联系方式">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="企业邮箱">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="经营状态">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="邮政编码">
+          <Input v-model="info.COMPANYPASSWORD"></Input>
+        </FormItem>
+        <FormItem prop="COMPANYPASSWORD" label="商户号">
           <Input v-model="info.COMPANYPASSWORD"></Input>
         </FormItem>
       </Form>
@@ -20,9 +62,9 @@
     </Panel>
   </Collapse>
   <div class="footer">
-    <Button type="success" :disabled="false" @click="">编辑</Button>
-    <Button :disabled="false" @click="">取消编辑</Button>
-    <Button type="primary" :disabled="false" @click="">保存</Button>
+    <Button v-show="!editAble" type="success" :disabled="false" @click="editAble= true">编辑</Button>
+    <Button v-show="editAble" :disabled="false" @click="editAble= false">取消编辑</Button>
+    <Button type="primary" :disabled="!editAble" @click="">保存</Button>
   </div>
 </div>
 </template>
@@ -38,7 +80,8 @@
         },
         ruleValidate:{
 
-        }
+        },
+        editAble: false
       }
     }
 	}
