ad2956049ad5eafb1ee18dbd07ff902d4d8198e4
lanto <lanto@lantodeMacBook.local>
2018-09-04 18:07:40 +0800

    store-info

--- src/hxx-components/store-info-detail.vue
+++ src/hxx-components/store-info-detail.vue
@@ -1,68 +1,128 @@
 <!--门店信息详情-->
 <template>
 <div id="store-info-detail">
-  <Collapse v-model="collapse">
-    <Panel name="1">门店基本信息
-      <Form class="common-form" ref="form" :rules="ruleValidate" :model="info" :label-width="80" slot="content">
-        <FormItem prop="COMPANYCODE" label="门店名称">
-          <Input v-model="info.COMPANYCODE"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYCODE" label="门店所属区域">
-          <Input v-model="info.COMPANYCODE"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="门店地址">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="经营业务类别">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="经营范围">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="统一社会信用代码">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="道路运输许可证号">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="许可证开始日期">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="许可证结束日期">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="负责人姓名">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="负责人联系方式">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="联系人姓名">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="联系人联系方式">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="企业邮箱">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="经营状态">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="邮政编码">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-        <FormItem prop="COMPANYPASSWORD" label="商户号">
-          <Input v-model="info.COMPANYPASSWORD"></Input>
-        </FormItem>
-      </Form>
-    </Panel>
-    <Panel name="2">证照上传
-
-    </Panel>
-  </Collapse>
+  <div class="info">
+    <Collapse v-model="collapse">
+      <Panel name="1">门店基本信息
+        <Form class="common-form" ref="form" :rules="ruleValidate" :model="info" :label-width="80" slot="content">
+          <FormItem prop="TENANT_NAME" label="门店名称">
+            <Input v-model="info.TENANT_NAME" :readonly="!editAble" :disabled="!isAdmin"></Input>
+            <!--<span class="formText" v-show="editAble">{{info.COMPANYCODE}}</span>-->
+          </FormItem>
+          <FormItem prop="TENANT_AREA_DISPLAY" label="门店所属区域">
+            <Input v-model="info.TENANT_AREA_DISPLAY" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="TENANT_ADD" label="门店地址">
+            <Input v-model="info.TENANT_ADD" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="BUSINESS_TYPE" label="经营业务类别">
+            <!--<Input v-model="info.BUSINESS_TYPE"></Input>-->
+            <Select v-model="info.BUSINESS_TYPE" :clearable="editAble"  :disabled="!editAble">
+              <Option v-for="(item, index) in BUSINESS_TYPE_group"
+                      :key="index" :value="item.code">{{item.name}}</Option>
+            </Select>
+          </FormItem>
+          <FormItem prop="COMPANY_BUSINESS_SCOPE" label="经营范围">
+            <Input v-model="info.COMPANY_BUSINESS_SCOPE" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="ORG_NUMBER" label="统一社会信用代码">
+            <Input v-model="info.ORG_NUMBER" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="ROAD_LICENSE" label="道路运输许可证号">
+            <Input v-model="info.ROAD_LICENSE" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="LICENSE_START_DATE" label="许可证开始日期">
+            <!--<Input v-model="info.LICENSE_START_DATE"></Input>-->
+            <DatePicker type="date" :value="info.LICENSE_START_DATE" :readonly="!editAble"></DatePicker>
+          </FormItem>
+          <FormItem prop="LICENSE_END_DATE" label="许可证结束日期">
+            <!--<Input v-model="info.LICENSE_END_DATE"></Input>-->
+            <DatePicker type="date" :value="info.LICENSE_END_DATE" :readonly="!editAble"></DatePicker>
+          </FormItem>
+          <FormItem prop="LEGAL_NAME" label="负责人姓名">
+            <Input v-model="info.LEGAL_NAME" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="LEGAL_TEL" label="负责人联系方式">
+            <Input v-model="info.LEGAL_TEL" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="LINK_MAN" label="联系人姓名">
+            <Input v-model="info.LINK_MAN" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="LINK_TEL" label="联系人联系方式">
+            <Input v-model="info.LINK_TEL" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="EMAIL" label="企业邮箱">
+            <Input v-model="info.EMAIL" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="STATUS" label="经营状态">
+            <!--<Input v-model="info.STATUS"></Input>-->
+            <Select v-model="info.STATUS" :clearable="editAble" :disabled="!editAble">
+              <Option v-for="(item, index) in STATUS_group"
+                      :key="index" :value="item.code">{{item.name}}</Option>
+            </Select>
+          </FormItem>
+          <FormItem prop="LINK_ZIP" label="邮政编码">
+            <Input v-model="info.LINK_ZIP" :readonly="!editAble"></Input>
+          </FormItem>
+          <FormItem prop="TENANT_NUM" label="门店商户号">
+            <Input v-model="info.TENANT_NUM" :readonly="!editAble" :disabled="!isAdmin"></Input>
+          </FormItem>
+        </Form>
+      </Panel>
+      <Panel name="2">证照上传
+        <div slot="content">
+          <Card class="pic-card">
+            <p slot="title"><i class="red" v-if="!isAdmin">*</i>上传道路运输许可证图片</p>
+            <div class="pic-body">
+              <img v-show="!info.ROAD_FILE_PATH" class="no-pic" src="../assets/images/no_img.png"/>
+              <img v-show="info.ROAD_FILE_PATH" class="pic" :src="info.ROAD_FILE_PATH"
+                   @click="showImg(info.ROAD_FILE_PATH)"/>
+              <div class="button" v-show="editAble">
+                <div class="up-img">
+                  <Button type="primary">上传</Button>
+                  <input type="file" accept="image/jpg,image/jpeg,image/png,image/bmp"
+                         @change="getImg('ROAD_FILE_PATH', $event)"/>
+                </div>
+                <Button type="error" @click="removeImg('ROAD_FILE_PATH')">移除</Button>
+              </div>
+            </div>
+          </Card>
+          <Card class="pic-card">
+            <p slot="title"><i class="red" v-if="!isAdmin">*</i>上传营业执照图片</p>
+            <div class="pic-body">
+              <img v-show="!info.BUS_FILE_PATH" class="no-pic" src="../assets/images/no_img.png"/>
+              <img v-show="info.BUS_FILE_PATH" class="pic" :src="info.BUS_FILE_PATH"/>
+              <div class="button" v-show="editAble">
+                <div class="up-img">
+                  <Button type="primary">上传</Button>
+                  <input type="file" accept="image/jpg,image/jpeg,image/png,image/bmp"
+                         @change="getImg('BUS_FILE_PATH',$event)"/>
+                </div>
+                <Button type="error" @click="removeImg('BUS_FILE_PATH')">移除</Button>
+              </div>
+            </div>
+          </Card>
+          <Card class="pic-card">
+            <p slot="title">上传门头图片</p>
+            <div class="pic-body">
+              <img v-show="!info.TENANT_FILE_PATH" class="no-pic" src="../assets/images/no_img.png"/>
+              <img v-show="info.TENANT_FILE_PATH" class="pic" :src="info.TENANT_FILE_PATH"/>
+              <div class="button" v-show="editAble">
+                <div class="up-img">
+                  <Button type="primary">上传</Button>
+                  <input type="file" accept="image/jpg,image/jpeg,image/png,image/bmp"
+                         @change="getImg('TENANT_FILE_PATH', $event)"/>
+                </div>
+                <Button type="error" @click="removeImg('TENANT_FILE_PATH')">移除</Button>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </Panel>
+    </Collapse>
+  </div>
   <div class="footer">
-    <Button v-show="!editAble" type="success" :disabled="false" @click="editAble= true">编辑</Button>
+    <Button v-show="!editAble" type="info" :disabled="false" @click="editAble= true">编辑</Button>
     <Button v-show="editAble" :disabled="false" @click="editAble= false">取消编辑</Button>
     <Button type="primary" :disabled="!editAble" @click="">保存</Button>
   </div>
@@ -70,18 +130,88 @@
 </template>
 
 <script>
+  import { getName, getDictGroup, imgToBase64 } from '@/libs/util.js'
 	export default {
 		name: "store-info-detail",
+    props: ['data'],
     data(){
 		  return{
         collapse: ['1', '2'],
         info:{
-
+          "TENANT_ID":"",
+          "TENANT_AREA_CODE":"",
+          "TENANT_NAME":"",
+          "TENANT_AREA_DISPLAY":"",
+          "TENANT_ADD":"",
+          "BUSINESS_TYPE":"",
+          "COMPANY_BUSINESS_SCOPE":"",
+          "ORG_NUMBER":"",
+          "ROAD_LICENSE":"",
+          "LICENSE_START_DATE":"",
+          "LICENSE_END_DATE":"",
+          "LEGAL_NAME":"",
+          "LEGAL_TEL":"",
+          "LINK_MAN":"",
+          "LINK_TEL":"",
+          "EMAIL":"",
+          "STATUS":"",
+          "LINK_ZIP":"",
+          "TENANT_NUM":"",
+          "ROAD_FILE_PATH":"",
+          "BUS_FILE_PATH":"",
+          "TENANT_FILE_PATH":""
         },
-        ruleValidate:{
 
-        },
+        ruleValidate:{},
         editAble: false
+      }
+    },
+    computed:{
+      BUSINESS_TYPE_group(){
+        return getDictGroup(this.$store.state.app.dict, '1033');
+      },
+      STATUS_group(){
+        return getDictGroup(this.$store.state.app.dict, '1034');
+      },
+      isAdmin(){
+        return this.$store.getters.loginType=='1001'
+      }
+    },
+    created(){
+      let rule= (this.isAdmin? []: [{ required: true, message:'必填项不能为空'}])
+      this.ruleValidate= {
+        TENANT_NAME: rule,
+        TENANT_AREA_DISPLAY:rule,
+        TENANT_ADD: rule,
+        BUSINESS_TYPE: [{ required: true, message:'必填项不能为空'}],
+        COMPANY_BUSINESS_SCOPE: rule,
+        ORG_NUMBER: rule,
+        ROAD_LICENSE: rule,
+        LICENSE_START_DATE: rule,
+        LICENSE_END_DATE: rule,
+        LEGAL_NAME: rule,
+        LEGAL_TEL: rule,
+        LINK_ZIP: rule,
+        ROAD_FILE_PATH: rule,
+        BUS_FILE_PATH: rule,
+      }
+    },
+    methods:{
+      getImg( name, e){
+        imgToBase64(e.target.files[0], (base64, fileName)=> {
+          this.info[name]= base64
+        })
+      },
+      showImg(img){
+        this.$Modal.info({
+          width: 90,
+          title: '查看',
+          closable: true,
+          content: '<img src="'+img+'" style="width: 100%"/>'
+        })
+      },
+      removeImg(name){
+        this.info[name]= ''
       }
     }
 	}
@@ -93,14 +223,99 @@
   overflow: auto;
   position: relative;
   background-color: white;
+  /*.formText{*/
+    /*display: block;*/
+    /*height: 33px;*/
+    /*padding: 0 7px;*/
+  /*}*/
+  .info{
+    width: 100%;
+    height: 100%;
+    padding-bottom: 60px;
+    overflow: auto;
+    .ivu-collapse{
+      border-bottom: 0;
+      min-height: 100%;
+      background-color: white;
+    }
+    .pic-card{
+      display: inline-block;
+      margin: 0 10px 10px 0;
+      width: 28%;
+      min-width: 250px;
+      .red{
+        color: red;
+      }
+      .pic-body{
+        width: 100%;
+        height: 200px;
+        position: relative;
+        .no-pic{
+          width: 250px;
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50% , -50%);
+        }
+        .pic{
+          max-width: 100%;
+          max-height: 100%;
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50% , -50%);
+          cursor: pointer;
+        }
+        .button{
+          width: 100%;
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          text-align: center;
+          > *{
+            margin: 0 5px;
+            vertical-align: top;
+          }
+          .up-img{
+            display: inline-block;
+            overflow: hidden;
+            position: relative;
+            input{
+              width: 100%;
+              height: 100%;
+              position: absolute;
+              left: 0;
+              top: 0;
+              opacity: 0;
+              font-size: 0;
+              cursor: pointer;
+            }
+          }
+        }
+      }
+    }
+    .pic-card:last-child{
+      margin-right: 0;
+    }
+  }
   .footer{
     width: 100%;
-    border-top: 1px solid #e8eaec;
+    border: 1px solid #e8eaec;
     padding: 12px 18px 12px 18px;
     text-align: right;
     position: absolute;
     left: 0;
     bottom: 0;
+    background-color: white;
+  }
+}
+</style>
+<style lang="less">
+#store-info-detail{
+  .ivu-collapse{
+    .ivu-collapse-header{
+      background-color: #f7f7f7;
+    }
   }
 }
 </style>
--- src/libs/util.js
+++ src/libs/util.js
@@ -427,3 +427,65 @@ export const deepClone = (data) => {
   }
   return obj;
 }
+
+/**
+ * @param {thisfile} 图片文件
+ * @param {callBack} 回调函数
+ * @returns {base64, filename}
+ * @description 图片压缩并转base64
+ */
+export const imgToBase64 = (thisfile, callBack) => {
+  // var file= $(domName).get(0).files[0]
+  var file= thisfile
+  var reader = new FileReader();
+  reader.readAsDataURL(file)
+  reader.onload = function (e) {
+    var image = new Image();
+    var self= this
+    image.src= e.target.result;
+    image.onload=function(){
+      var width = image.width;
+      var height = image.height;
+      _compress(self.result,
+        {width: width, height:height, quality: 0.6, type: file.type} ,
+        file.name,
+        callBack
+      )
+    };
+  }
+}
+export const _compress = (path, obj, name, callBack) => {
+  var img = new Image();
+  img.src = path;
+  img.onload = function () {
+    var that = this;
+    // 默认按比例压缩
+    var w = that.width,
+      h = that.height,
+      scale = w / h;
+    w = obj.width || w;
+    h = obj.height || (w / scale);
+    var quality = 0.7;  // 默认图片质量为0.7
+    //生成canvas
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    // 创建属性节点
+    var anw = document.createAttribute("width");
+    anw.nodeValue = w;
+    var anh = document.createAttribute("height");
+    anh.nodeValue = h;
+    canvas.setAttributeNode(anw);
+    canvas.setAttributeNode(anh);
+    ctx.drawImage(that, 0, 0, w, h);
+    // 图像质量
+    if (obj.quality && obj.quality <= 1 && obj.quality > 0) {
+      quality = obj.quality;
+    }
+    // quality值越小，所绘制出的图像越模糊
+    var base64 = canvas.toDataURL(obj.type|| 'image/png', quality);
+    // console.log(base64)
+    // 返回base64的值
+    callBack(base64, name )
+  }
+
+}
