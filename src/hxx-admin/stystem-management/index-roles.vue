72af537125848673f272a4f4ad3f3528bcad3713
xieruixiang <1046421940@qq.com>
2018-10-17 20:59:17 +0800

    Signed-off-by: xieruixiang <1046421940@qq.com>

--- src/hxx-admin/stystem-management/index-roles.vue
+++ src/hxx-admin/stystem-management/index-roles.vue
@@ -1,38 +1,111 @@
 <template>
-    <div>
-      <xunit-tree v-for="item  in treeData" :level="1" :indexId="0" :text="item.text" :funcId="item.funcId" :data="item.children" :checked="item.checked"></xunit-tree>
-    </div>
+  <div>
+    <xunit-tree :expand="true" v-for="item  in treeData" :level="1" :indexId="0" :text="item.text"
+                @checkTrue="checkTrue" :funcId="item.funcId" @checkFalse="checkFalse" :data="item.children"
+                :checked="item.checked" @pushIds="pushIds" @spliceId="spliceId"></xunit-tree>
+  </div>
 </template>
 <script>
   import xunitTree from '@/hxx-components/xunit-tree.vue'
-    export default {
-        name: "index-roles",
-        components:{xunitTree},
-        methods:{
-          getTree(){
-            this.axios.request({
-              url: '/manage/sys/tenant_roles/getCheckedMenus',
-              method: 'post',
-              data: {
-                access_token: this.$store.state.user.token,
-                ROLE_ID:37,
-              }
-            }).then(res => {
-              if (res.success === true) {
-                  this.treeData = [res.data];
-              }
-            })
+
+  export default {
+    name: "index-roles",
+    components: {xunitTree},
+    methods: {
+      pushIds(data){
+        // console.log(data);
+       let mydata =  this.findId(this.treeData,data);
+       this.treeData = mydata;
+      },
+      spliceId(data){
+        // let data = this.spliceId(data);
+      },
+      findId(data,ids){
+        for(let i in data){
+          let flag = data[i].children ? true : false;
+          if(data[i].funcId == ids[0]){
+            data[i].checked = true;
+            ids.splice(0,1);
+            if(flag) this.findId(data[i].children,ids);
+          }else{
+           if(flag) this.findId(data[i].children,ids);
           }
-        },
-        data(){
-          return {
-            treeData:[],
+        }
+        return data;
+      },
+      changeData(data) {
+        data.expand = true;
+        let flag = data.children ? true : false;
+        if (flag) {
+          for (let i in data.children) {
+            this.changeData(data.children[i]);
           }
-        },
-      mounted(){
-          this.getTree();
+        }
+        return data;
+      },
+      getTree() {
+        this.axios.request({
+          url: '/manage/sys/tenant_roles/getCheckedMenus',
+          method: 'post',
+          data: {
+            access_token: this.$store.state.user.token,
+            ROLE_ID: 36,
+          }
+        }).then(res => {
+          if (res.success === true) {
+            let data = this.changeData(res.data);
+            this.treeData = [data];
+          }
+        })
+      },
+      checkTrue(id) {
+        let data = this.setTrue(this.treeData, id);
+        this.treeData = data;
+      },
+      checkFalse(id) {
+        let data = this.setFalse(this.treeData, id);
+        this.treeData = data;
+      },
+      setFalse(data, id) {
+        for (let i in data) {
+          let flag = data[i].children ? true : false;
+          if (data[i].funcId == id) {
+            data[i].checked = false;
+            if (flag) this.setAll(data[i].children, false)
+          } else {
+            if (flag) this.setFalse(data[i].children, id);
+          }
+        }
+        return data;
+      },
+      setTrue(data, id) {
+        for (let i in data) {
+          let flag = data[i].children ? true : false;
+          if (data[i].funcId == id) {
+            data[i].checked = true;
+            if (flag) this.setAll(data[i].children, true)
+          } else {
+            if (flag) this.setTrue(data[i].children, id);
+          }
+        }
+        return data;
+      }, setAll(data, type) {
+        for (let i in data) {
+          data[i].checked = type;
+          let flag = data[i].children ? true : false;
+          if (flag) this.setAll(data[i].children, type);
+        }
       }
+    },
+    data() {
+      return {
+        treeData: [],
+      }
+    },
+    mounted() {
+      this.getTree();
     }
+  }
 </script>
 <style scoped>
 
--- src/hxx-components/xunit-tree.vue
+++ src/hxx-components/xunit-tree.vue
@@ -2,9 +2,11 @@
   <div>
     <div style="background:none;cursor: pointer;line-height:25px;">
       <div style="width:30%;text-align:left;float:left;">
-        <div ref="abc"  style="width:20px;height:20px;float:left;"></div>
-        <div style="cursor:pointer;width:15px;height:15px;float:left;" >▼</div>
-        <div style="float:left"><Checkbox :value="checked"></Checkbox></div>
+        <div ref="abc" style="width:20px;height:20px;float:left;"></div>
+        <div style="cursor:pointer;width:15px;height:15px;float:left;">{{arrows}}</div>
+        <div style="float:left">
+          <Checkbox @on-change="onChange" :value="checked"></Checkbox>
+        </div>
         <div style="float:left;">{{text}}</div>
       </div>
       <div style="width:40%;float:left;text-align:center;">功能权限</div>
@@ -14,7 +16,8 @@
       <div style="clear:both;"></div>
     </div>
     <unit-tree v-if="indexId < 0"></unit-tree>
-    <xunit-tree v-for="item in data" :indexId="0" :text="item.text" :funcId="item.funcId" :level="level+1" :data="item.children" :checked="item.checked"></xunit-tree>
+    <xunit-tree v-for="item in data" :expand="item.expand" :indexId="0" :text="item.text" :funcId="item.funcId" v-show="item.expand"
+                :level="level+1" :data="item.children" @spliceId="spliceId" :checked="item.checked" @checkTrue="checkTrue" @pushIds="pushIds" @checkFalse="checkFalse"></xunit-tree>
   </div>
 </template>
 <script>
@@ -23,13 +26,53 @@
   export default {
     name: "xunit-tree",
     components: {xunitTree},
-    props: ['indexId','text','funcId','data','checked','level'],
+    props: ['indexId', 'text', 'funcId', 'data', 'checked', 'level','expand'],
     data() {
-      return {}
+      return {
+        // arrows: '▶',
+      }
     },
-    mounted(){
-      console.log(this.level);
+    mounted() {
       this.$refs.abc.style.width = (this.level - 1) * 20 + "px";
+    },
+    methods:{
+      onChange(type){
+        if(type){
+          this.$emit('checkTrue',this.funcId);
+          this.$emit('pushIds',[this.funcId]);
+        }else{
+          this.$emit('checkFalse',this.funcId);
+          this.$emit('spliceId',[this.funcId]);
+        }
+      },
+      checkTrue(row){
+        this.$emit('checkTrue',row);
+      },
+      checkFalse(row){
+        this.$emit('checkFalse',row);
+      },
+      spliceId(ids){
+        ids.push(this.funcId);
+        this.$emit('spliceId',ids);
+      },
+      pushIds(ids){
+        ids.unshift(this.funcId);
+        this.$emit('pushIds',ids);
+      },
+    },
+    computed:{
+     arrows(){
+       // console.log(JSON.stringify(this.data[0].children));
+       if(this.data){
+         if(this.expand){
+           return '▼';
+         }else{
+           return '▶';
+         }
+       }else{
+         return '';
+       }
+      }
     },
   }
 </script>
