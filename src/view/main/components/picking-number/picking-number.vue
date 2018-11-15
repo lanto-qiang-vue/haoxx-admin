1e392b92b0e5266308a4c1118981a99df3e66218
xieruixiang <1046421940@qq.com>
2018-11-15 13:18:34 +0800

    Signed-off-by: xieruixiang <1046421940@qq.com>

--- src/view/main/components/picking-number/picking-number.vue
+++ src/view/main/components/picking-number/picking-number.vue
@@ -1,5 +1,5 @@
 <template>
-<Tooltip class="picking-number" content="维修领料" v-if="showThis" >
+<Tooltip class="picking-number" v-show="showRule" content="维修领料" v-if="showThis" >
 <div class="text" @click="goTo">
   <span>维修领料(</span><span class="red">{{num}}</span><span>)</span>
 </div>
@@ -10,6 +10,11 @@
   import { mapActions } from 'vuex'
 	export default {
 		name: "picking-number",
+    data(){
+		  return {
+		    showRule:false,
+      }
+    },
     computed:{
 		  num(){
         return this.$store.state.app.pickingNumber || 0;
@@ -19,12 +24,32 @@
       }
     },
     mounted(){
-      if(this.showThis) this.getPickingNumber()
+      if(this.showThis) this.getPickingNumber();
+      let data = this.$store.state.user.accessMenu;
+      let store = this.getFuncId(data,'100201',[]);
+      if(store.length == 1){
+        this.showRule = true;
+      }else{
+        this.showRule = false;
+      }
     },
     methods: {
       ...mapActions([
         'getPickingNumber'
       ]),
+      getFuncId(data,id,store){
+        for(let i in data){
+          if(data[i].funcId == id){
+            store.push(id);
+            break;
+          }else{
+            if(data[i].children){
+              this.getFuncId(data[i].children,id,store)
+            }
+          }
+        }
+        return store;
+      },
       goTo(){
         this.$router.push('/maintain-pick')
       }
