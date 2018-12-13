ab2b0e2d7d3284fd043906471690a3ad7dec8686
xieruixiang <1046421940@qq.com>
2018-12-13 14:51:34 +0800

    Signed-off-by: xieruixiang <1046421940@qq.com>

--- src/hxx-store/customer-relations/iframe.vue
+++ src/hxx-store/customer-relations/iframe.vue
@@ -1,12 +1,17 @@
 <template>
-    <div style="width:100%;height:100%;">
-      <iframe src="http://mall.hoxiuxiu.com/" style="height:100%;width:100%;"></iframe>
+    <div style="width:100%;height:100%;overflow: hidden">
+      <iframe :src="myurl" style="height:100%;width:100%;"></iframe>
     </div>
 </template>
 
 <script>
     export default {
-        name: "iframe"
+        name: "iframe",
+        data(){
+          return {
+            myurl:'http://localhost:4040/dq/login/hxx_login?token='+ this.$store.state.user.token + "&mobile=17321492645",
+          }
+        }
     }
 </script>
 
