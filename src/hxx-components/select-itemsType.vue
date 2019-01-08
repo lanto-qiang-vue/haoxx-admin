0ca6b740de626367dbe5a183ddd543ff7dd4271f
xchen <xchen@lantoev.com>
2019-01-08 12:25:24 +0800

    ''

--- src/hxx-components/select-itemsType.vue
+++ src/hxx-components/select-itemsType.vue
@@ -463,8 +463,16 @@ import commonTable from '@/hxx-components/common-table.vue'
                             }
                         }).then(res => {
                             if (res.success === true) {
+                                if(res.data.length>0){
+                                    this.addFun();
+                                }else{
+                                    this.$Modal.confirm({
+                                        title:"系统提示!",
+                                        content:"当前门店未导入自定义类型，请联系客服！",
 
-                                this.addFun();
+                                    })
+                                }
+                                
 
 
 
