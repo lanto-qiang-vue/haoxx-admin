ab0689609ec9edf6dc8272cb8ef805ed8ddfdceb
xieruixiang <1046421940@qq.com>
2019-01-19 09:23:49 +0800

    Signed-off-by: xieruixiang <1046421940@qq.com>

--- src/hxx-components/LodopFuncs.js
+++ src/hxx-components/LodopFuncs.js
@@ -43,7 +43,7 @@ if (needCLodop()) {
   head.insertBefore( oscript,head.firstChild );
 
   oscript = document.createElement("script");
-	oscript.src ="http://localhost:8000/CLodopfuncs.js?priority=1";
+	oscript.src ="https://localhost:8000/CLodopfuncs.js?priority=1";
 	head.insertBefore( oscript,head.firstChild );
 
 	//引用双端口(8000和18000）避免其中某个被占用：
@@ -62,7 +62,7 @@ export const getLodop=function(oOBJECT,oEMBED){
     var strHtm64_Update="<br><font color='#0277bd'>打印控件需要升级!点击这里<a href='resources/lodop/install_lodop64.exe' style='color:#FF00FF;' target='_self'>执行升级</a>,升级后请重新进入。</font>";
     var strHtmFireFox="<br><br><font color='#0277bd'>（注意：如曾安装过Lodop旧版附件npActiveXPLugin,请在【工具】->【附加组件】->【扩展】中先卸它）</font>";
     var strHtmChrome="<br><br><font color='#0277bd'>(如果此前正常，仅因浏览器升级或重安装而出问题，需重新执行以上安装）</font>";
-    var strCLodopInstall="<br><p color='#0277bd'>CLodop云打印服务(localhost本地)未安装启动!点击这里<a href='http://www.lodop.net/download.html' style='color:#FF00FF;' target='_blank'>执行安装</a>,安装后请刷新页面。</p>";
+    var strCLodopInstall="<br><p color='#0277bd'>CLodop云打印服务(localhost本地)未安装启动!点击这里<b style='color:red;'>本站采用https安全协议。请下载安装云打印C-Lodop扩展版。请注意是扩展版本。火狐浏览器请拷贝https://localhost:8443/到浏览器链接栏添加安全列外。如有疑问请联系客服</b></b><a href='http://www.lodop.net/download.html' style='color:#FF00FF;' target='_blank'>执行安装</a>,安装后请刷新页面。</p>";
     var strCLodopUpdate="<br><font color='#0277bd'>CLodop云打印服务需升级!点击这里<a href='resources/lodop/CLodop_Setup_for_Win32NT.exe'  style='color:#FF00FF;' target='_self'>执行升级</a>,升级后请刷新页面。</font>";
     var LODOP;
     var obj
