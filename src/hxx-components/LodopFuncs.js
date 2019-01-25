b292cd72b017a3664a621c6daf1c4d8206594c0e
xieruixiang <1046421940@qq.com>
2019-01-25 17:14:02 +0800

    Signed-off-by: xieruixiang <1046421940@qq.com>

--- src/hxx-components/LodopFuncs.js
+++ src/hxx-components/LodopFuncs.js
@@ -39,16 +39,16 @@ function needCLodop(){
 if (needCLodop()) {
 	var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
 	var  oscript = document.createElement("script");
-  // oscript.src ="https://localhost:8443/CLodopfuncs.js";
+  oscript.src ="https://localhost:8443/CLodopfuncs.js";
   head.insertBefore( oscript,head.firstChild );
 
   oscript = document.createElement("script");
-	// oscript.src ="http://localhost:8000/CLodopfuncs.js?priority=1";
+	oscript.src ="http://localhost:8000/CLodopfuncs.js?priority=1";
 	head.insertBefore( oscript,head.firstChild );
 
 	//引用双端口(8000和18000）避免其中某个被占用：
 	oscript = document.createElement("script");
-	// oscript.src ="http://localhost:18000/CLodopfuncs.js?priority=0";
+	oscript.src ="http://localhost:18000/CLodopfuncs.js?priority=0";
 	head.insertBefore( oscript,head.firstChild );
 };
 
