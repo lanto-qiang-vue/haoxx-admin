a5f1d23a64a463f0c50615afd52cb5ff6f61babd
xieruixiang <rxxie@lantoev.com>
2019-04-23 12:54:17 +0800

    修改车牌正则

--- src/hxx-components/select-addVehicle.vue
+++ src/hxx-components/select-addVehicle.vue
@@ -171,7 +171,10 @@
                 },
                 ruleValidate:{
 					 PLATE_NUM:[{required: true, message: '车牌号必填', trigger: 'blur' },
-					 { type:'string',pattern:/^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1,2}$/, message:'请输入正确的车牌号码', trigger:'change'}
+					 { type:'string',
+             // pattern:/^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1,2}$/,
+             pattern:/^[^(~)|(!)|(！)|(@)|(#)|($)|(￥)|(%)|(^)|(&)|(*)|(/)|(:)|(：)|(;)|(；)|(.)|(。)|(,)|(，)|(?)|(？)|(")|(')|(\\)|({)|(})|(\[)|(\])|(\-)|(_)|(\+)|(=)|(`)|(<)|(>)|(、)|(《)|(》)]+$/,
+             message:'请输入正确的车牌号码', trigger:'change'}
 					 ],
 					 VIN_NO:[{required: true, message: '车架号必填', trigger: 'blur' },
 					 	{ validator: validatePass, trigger: 'change' },
