f916804f801e1c88b1814fa340cfdf76ce512545
xieruixiang <rxxie@lantoev.com>
2019-05-05 10:18:47 +0800

    话题圈页面

--- src/hxx-admin/topic-manage/topic-list.vue
+++ src/hxx-admin/topic-manage/topic-list.vue
@@ -11,12 +11,12 @@
                     @on-change="onChange"></DatePicker>
       </div>
       <div class="search-block">
-      <Select v-model="topicId" placeholder="请选择话题圈">
-        <Option v-for="(item, index) in topicList"
-                :key="index" :value="item.id">{{item.name}}
-        </Option>
-      </Select>
-    </div>
+        <Select v-model="topicId" placeholder="请选择话题圈">
+          <Option v-for="(item, index) in topicList"
+                  :key="index" :value="item.id">{{item.name}}
+          </Option>
+        </Select>
+      </div>
       <div class="search-block">
         <Select v-model="typeId" placeholder="请选择状态">
           <Option v-for="(item, index) in typeList"
@@ -41,20 +41,39 @@
     data() {
       return {
         topicId: '请选择话题圈',
-        typeId:'请选择状态',
+        typeId: '请选择状态',
         typeList: [
-          { id: '请选择状态', name: '请选择状态'},
+          {id: '请选择状态', name: '请选择状态'},
           {id: '1', name: '话题圈1'},
           {id: '2', name: '话题圈2'},
         ],
         topicList: [
-          { id: '请选择话题圈', name: '请选择话题圈'},
+          {id: '请选择话题圈', name: '请选择话题圈'},
           {id: '1', name: '话题圈1'},
           {id: '2', name: '话题圈2'},
         ],
         value: [],
         tableData: [],
-        columns: [],
+        columns: [
+          {
+          title: '序号', width: 100,
+          render: (h, params) => h('span', (this.page - 1) * this.limit + params.index + 1)
+        },
+          {title: '发布人', key: 'A', minWidth: 100},
+          {title: '话题', key: 'B', minWidth: 100},
+          {title: '话题圈', key: 'C', minWidth: 100},
+          {title: '发布时间', key: 'D', minWidth: 100},
+          {title: '评论数', key: 'E', minWidth: 100},
+          {title: '状态', key: 'E', minWidth: 100},
+          {title: '操作', key: 'E', minWidth: 100,
+          render:(h,params) => {
+            let button = [];
+            button.push(h('Button',{type:'primary'},'关闭/恢复'));
+            button.push(h('Button',{type:'primary',style:{marginLeft:'10px'}},'查看详情'));
+            return h('div',button);
+          }
+          },
+        ],
         total: 0,
         showTable: false,
         page: 1,
@@ -70,8 +89,12 @@
     },
     mounted() {
       this.showTable = Math.random();
+      this.getList();
     },
     methods: {
+      getList(){
+        this.tableData = [{A:'A'},{B:'B'}];
+      },
       onChange(val) {
         this.value = val;
         console.log(this.value);
