bbbb6d15f67f23fbd04e9c37dca7829615af5dba
xieruixiang <rxxie@lantoev.com>
2019-06-12 11:30:53 +0800

    update

---
+++ src/libs/fxios.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import store from '@/store'
+import { Message } from 'iview'
+import baseURL from '_conf/url'
+const fxios = axios.create({
+  baseURL:baseURL,
+  timeout: 1000,
+});
+
+fxios.interceptors.request.use(function (config) {
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+fxios.interceptors.response.use(function (response) {
+  console.log(response);
+  return response.data;
+}, function (error) {
+  // 对响应错误做点什么
+  console.log("error",error);
+  return Promise.reject(error);
+});
+export default fxios;
